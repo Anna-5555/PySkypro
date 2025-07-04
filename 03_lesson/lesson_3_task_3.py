7bfa9781f41dac2ca7e6d960acd126783f0b22b4
Anna-5555 <anna.khaybra@mail.ru>
2025-07-04 23:34:04 +0300

    Добавила ветку lesson3. Домашка по ОПП в Python.

---
+++ 03_lesson/lesson_3_task_3.py
@@ -0,0 +1,17 @@
+from address import Address
+from mailing import Mailing
+
+# Создаём два адреса
+to_address = Address("123456", "Москва", "Ленина", "10", "25")
+from_address = Address("654321", "Санкт-Петербург", "Пушкина", "5", "30")
+
+# Создаём почтовое отправление
+mail = Mailing(to_address, from_address, 1000, "ABC123")
+
+# Выводим данные в заданном формате
+print(f"Отправление {mail.track} из {mail.from_address.index},"
+      f" {mail.from_address.city}, {mail.from_address.street}, "
+      f"{mail.from_address.house} - {mail.from_address.apartment}"
+      f" в {mail.to_address.index}, {mail.to_address.city}, "
+      f"{mail.to_address.street}, {mail.to_address.house} - "
+      f"{mail.to_address.apartment}. Стоимость {mail.cost} рублей.")
